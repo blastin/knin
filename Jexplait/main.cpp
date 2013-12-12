c74665851a7106b1f782098b424eadee045badb9
jeff <jeff@jeff-P45T-A>
2013-12-12 01:51:16 -0200

    Inicialização Jexplait

---
+++ Jexplait/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: jeff
+ *
+ * Created on 12 de Dezembro de 2013, 01:50
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
