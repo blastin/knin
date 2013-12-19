613e0dd362ba89e569b7629003f818fd8ec6ce3e
blastin <anarquia.suporte@gmail.com>
2013-12-19 20:08:16 -0200

    add

---
+++ Jexplait/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jeff
+ *
+ * Created on 19 de Dezembro de 2013, 20:06
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
